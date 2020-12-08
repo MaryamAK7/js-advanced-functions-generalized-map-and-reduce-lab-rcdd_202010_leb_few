e48b1be8987e1e16a76bbf90974e1e44f9e1ea82
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2020-12-08 15:49:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ function map(arr, fct){
 }
 
 function reduce(arr, fct, start){
- let r= ()
+ let r= (!!start) ? start : arr[0];
+ let i= (!!start) ? 0 : 1;
+ for (let i=0; i< arr.length;i++){
+    r = cb(src[i], r)
+ }
  
- 
+ return r;
 }
